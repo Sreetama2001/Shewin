99ab7d977cd501ab80f5339f2eb88edf1fa47678
MSamiDev <mohammadsami@duck.com>
2023-01-08 15:17:44 +0530

    Update : chatbot

--- src/ChatbotAPI.js
+++ src/ChatbotAPI.js
@@ -1,67 +1,82 @@
 import axios from "axios";
+import { useState } from "react";
 // import fetch  from 'node-fetch';
 async function moodGuesser(message) {
-	// http://tweetmoodchk.d7cgcshfajbxf6dv.eastus.azurecontainer.io/mood
+	// const response = axios
+	// 	.post("http://127.0.0.1:8000/mood",
 
-	
+	// 		 message
 
-	const response = axios
-		.get("https://official-joke-api.appspot.com/random_joke")
+	// 		// Headers: {
+	// 		// 	"Access-Control-Allow-Origin": "*",
+
+	// 		// 	"Access-Control-Allow-Headers":
+	// 		// 	"Origin, X-Requested-With, Content-Type, Accept",
+	// 		// 	"Content-Type": "application/json",
+	// 		// },
+	// 	)
+	// 	.then(function (response) {
+	// 		// handle success
+	// 		console.log(response.data);
+	// 	})
+	// 	.catch(function (error) {
+	// 		// handle error
+	// 		console.log(error);
+	// 	})
+	// 	.then(function () {
+	// 		// always executed
+	// 	});
+
+	// const options = {
+	// 	url: "http://127.0.0.1:8000/mood",
+	// 	method: "POST",
+	// 	headers: {
+	// 		Accept: "application/json",
+	// 		"Content-Type": "application/json;charset=UTF-8",
+	// 	},
+	// 	data: {
+	// 		input_mood: "I am not feeling good",
+	// 	},
+	// };
+
+	// const response = axios(options).then((response) => {
+	// 	console.log(response.status);
+	// });
+
+	const [data, setData] = useState("");
+	const config = {
+		method: "post",
+		url: `http://localhost:8000/mood?input_mood=${message}`,
+		headers: {},
+	};
+
+	const response = axios(config)
 		.then(function (response) {
-			// handle success
-			console.log(response.data);
+			console.log(JSON.stringify(response.data));
+			// console.log(JSON.stringify(response.data.reply));
+			setData(response.data.reply);
 		})
 		.catch(function (error) {
-			// handle error
 			console.log(error);
-		})
-		.then(function () {
-			// always executed
 		});
 
-	return response.data.setup + response.data.punchline;
-}
-
-async function getMeme() {
-	const response = axios
-		.get("https://meme-api.herokuapp.com/gimme/IndianDankMemes")
-		.then(function (response) {
-			// handle success
-			console.log(response.data);
-		})
-		.catch(function (error) {
-			// handle error
-			console.log(error);
-		})
-		.then(function () {
-			// always executed
-		});
-
-	return response.data.setup + response.data.punchline;
+	return data;
 }
 
 const API = {
+
 	GetChatbotResponse: async (message) => {
 		return new Promise(function (resolve, reject) {
 			setTimeout(function () {
 				if (message === "hi" || message === "hello" || message === "hey")
 					resolve(
-						"Here I am  your mood guesser bot , I will send you jokes which will make you happy! Plese type 'joke'. "
+						"Here I am  your mood guesser bot , I will send you jokes which will make you happy!. "
 					);
-				else if (message === "joke") {
-					axios
-						.get("https://official-joke-api.appspot.com/random_joke")
-						.then(function (response) {
-							// handle success
-							console.log(response.data);
-							resolve(response.data.setup + " " + response.data.punchline);
-						})
-						.catch(function (error) {
-							// handle error
-							console.log(error);
-						});
-				} else {
-					resolve("Here I am  your mood guesser bot , I will send you jokes which will make you happy! Plese type 'joke'. ");
+				else {
+					const data = moodGuesser(message);
+					console.log();
+					// resolve();
+					// console.log(moodGuesser(message));
 				}
 			}, 1000);
 		});
